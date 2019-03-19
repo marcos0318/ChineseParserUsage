40776c9e64d42ecfbe8cca5f342b97c9de1a7136
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 22:51:42 +0800

    debug

--- segmentation.py
+++ segmentation.py
@@ -1,3 +1,3 @@
 import os
 
-os.system['/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out]
+os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
