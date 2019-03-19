018421418beb71e958d5e7101c8f335cda101a69
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:29:11 +0800

    debug

--- segmentation.py
+++ segmentation.py
@@ -3,26 +3,19 @@ import os
 targetPath = '/home/data/corpora/wikipedia/chinese-wiki/' 
 firstNames = os.listdir(targetPath)
 
-files = []
+
+
+
 
 for firstName in firstNames:
     fileNames = os.listdir(targetPath + firstName + '/')
 
-    for name in fileNames:
+    def parseFile(name):
         finalPath = targetPath + firstName + '/' + name
-
-        files.append(finalPath)
-
-print(len(files))
-
-for firstName in firstNames:
-    fileNames = os.listdir(targetPath + firstName + '/')
-
-    for name in fileNames:
-        finalPath = targetPath + firstName + '/' + name
-
-        files.append(finalPath)
-
         os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku ' + finalPath + ' UTF-8 0 > /home/data/corpora/wikipedia/parsedChineseWiki/' + firstName + name)
+
+    for name in fileNames:
+        parseFile(name)
+        
         break
     break
