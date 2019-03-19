171a6683a3f2d3d4be2f495cdb4f7bd0f09e870b
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:34:34 +0800

    run multi

--- segmentation.py
+++ segmentation.py
@@ -1,21 +1,20 @@
 import os
+from multiprocessing import Pool
 
-targetPath = '/home/data/corpora/wikipedia/chinese-wiki/' 
-firstNames = os.listdir(targetPath)
+if __name__ == '__main__':
+    targetPath = '/home/data/corpora/wikipedia/chinese-wiki/' 
+    firstNames = os.listdir(targetPath)
 
 
 
+    for firstName in firstNames:
+        fileNames = os.listdir(targetPath + firstName + '/')
 
+        def parseFile(name):
+            finalPath = targetPath + firstName + '/' + name
+            os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku ' + finalPath + ' UTF-8 0 > /home/data/corpora/wikipedia/parsedChineseWiki/' + firstName + name)
 
-for firstName in firstNames:
-    fileNames = os.listdir(targetPath + firstName + '/')
+        with Pool(processes=20) as pool:
+            pool.map(parseFile, fileNames)
 
-    def parseFile(name):
-        finalPath = targetPath + firstName + '/' + name
-        os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku ' + finalPath + ' UTF-8 0 > /home/data/corpora/wikipedia/parsedChineseWiki/' + firstName + name)
-
-    for name in fileNames:
-        parseFile(name)
-        
-        break
-    break
+       
