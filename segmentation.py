aa1dfaecdf7f713222801cd9e7bfff9920c3fff1
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:05:38 +0800

    deb7ug

--- segmentation.py
+++ segmentation.py
@@ -1,3 +1,8 @@
 import os
 
-os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
+targetPath = '/home/data/corpora/wikipedia/chinese-wiki/' 
+firstNames = os.listdir(targetPath)
+
+print(firstNames)
+
+# os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
