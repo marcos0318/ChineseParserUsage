ac03861ca37b102154775ca456ab0757cb4fa77a
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:11:39 +0800

    debug

--- segmentation.py
+++ segmentation.py
@@ -5,4 +5,13 @@ firstNames = os.listdir(targetPath)
 
 print(firstNames)
 
+
+for firstName in firstNames:
+    fileNames = os.listdir(targetPath + firstName + '/')
+
+    for name in fileNames:
+        finalPath = targetPath + firstName + '/' + name
+        print(finalPath)
+
+
 # os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
