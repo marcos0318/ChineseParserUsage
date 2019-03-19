09eeccdf81259f795803f98e4a0567e9599e5d9b
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:21:48 +0800

    test seg

--- segmentation.py
+++ segmentation.py
@@ -13,10 +13,9 @@ for firstName in firstNames:
 
     for name in fileNames:
         finalPath = targetPath + firstName + '/' + name
-        print(finalPath)
+
         files.append(finalPath)
 
-print(files)
-
-
-# os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
+        os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku ' + finalPath + ' UTF-8 0 > /home/data/corpora/wikipedia/parsedChineseWiki/' + firstName + name)
+        break
+    break
