1b272f0dc022d5d2170bd3b6bac20ce826fd6678
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-19 23:15:28 +0800

    debug

--- segmentation.py
+++ segmentation.py
@@ -6,12 +6,17 @@ firstNames = os.listdir(targetPath)
 print(firstNames)
 
 
+files = []
+
 for firstName in firstNames:
     fileNames = os.listdir(targetPath + firstName + '/')
 
     for name in fileNames:
         finalPath = targetPath + firstName + '/' + name
         print(finalPath)
+        files.append(finalPath)
+
+print(files)
 
 
 # os.system('/home/jbai/Parsers/stanford-segmenter-2018-10-16/segment.sh pku /home/jbai/Parsers/stanford-segmenter-2018-10-16/sample.in UTF-8 0 > sample.out')
