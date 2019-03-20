0a6d1849fb8609b1580a162b5601c7c3b0ba4f1f
Jiaxin_Bai <jbai0318@gmail.com>
2019-03-21 01:38:44 +0800

    debug

--- parseChineseWiki.py
+++ parseChineseWiki.py
@@ -25,7 +25,7 @@ def parse_sentense_with_stanford(input_sentence, nlp_id=0):
     # '我喜欢吃美味的寿司，不喜欢吃难吃的炸酱面' 
 
     # TODO: Replace the tmp output
-    request = """wget --post-data '""" + cleaned_sentence + """' 'localhost: """ + str(10001 + nlp_id) + """/?properties={"annotators":"tokenize,depparse,lemma","outputFormat":"json"}' -O - """
+    request = """wget --post-data '""" + cleaned_sentence + """' 'localhost:""" + str(10001 + nlp_id) + """/?properties={"annotators":"tokenize,depparse,lemma","outputFormat":"json"}' -O - """
 
     respondStr = subprocess.check_output(request, shell=True)
 
